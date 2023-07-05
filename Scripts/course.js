264152d10ac4cd24832559e446a3ba5bc9ee0e5a
ottawaAli <137909613+ottawaAli@users.noreply.github.com>
2023-07-04 21:39:37 -0400

    modify the js

--- Scripts/course.js
+++ Scripts/course.js
@@ -1,5 +1,6 @@
+// Course data
 const courses = {
-    "myCourses":[
+    "courses":[
         {"name":"Introduction to Computer Programming(Java)", "level":"1", "code":"CST7116", "description":"An introduction to computer programming with emphasis on problem analysis and design, using algorithms, pseudocode, flowcharts, UML class diagrams and testing, with the Java programming language used as a means to implement problem solution designs.", "image":"Lv1Java.jpg"},
         {"name":"Introduction to Database", "level":"1", "code":"CST7215", "description":"the fundamentals of relational databases design using Entity Relation Diagrams (ERDs), and use Structured Query Language (SQL) to create, modify and query a database. ", "image":"Lv1Data.jpg"},
         {"name":"Computer Essentials", "level":"1", "code":"CST7101", "description":"Maintaining a computer, using operating system and productivity software, and expressing related environmental and sustainability concerns.", "image":"Lv1CE.jpg"},
@@ -16,85 +17,49 @@ const courses = {
     ]
 }
 
-function filterCourses(event) {
-    // Get user's input 
-    let searchTerm = document.getElementById("query").value;
+function selectCourse(event) {
+    // Get user's input, using toLowerCase() to implement case-insensitive search
+    let searchTerm = document.getElementById("search-course").value.toLowerCase();
     let selectedLevel = document.getElementById("filter-level").value;
     let selectedSort = document.getElementById("sort-level").value;
 
-    // Store  courses
-    let filteredCourses = [];
-
-    // Select the level
-    switch (selectedLevel) {
-        case "All":
-            filteredCourses = courses.myCourses.filter(course => course.name.toLowerCase().includes(searchTerm.toLowerCase()));
-            break;
-        case "Level 1":
-            filteredCourses = courses.myCourses.filter(course => course.name.toLowerCase().includes(searchTerm.toLowerCase())
-                                                                && course.level === "1");
-            break;
-        case "Level 2":
-            filteredCourses = courses.myCourses.filter(course => course.name.toLowerCase().includes(searchTerm.toLowerCase())
-                                                                && course.level === "2");
-            break;																
-        case "Level 3":																
-            filteredCourses = courses.myCourses.filter(course => course.name.toLowerCase().includes(searchTerm.toLowerCase())
-                                                                && course.level === "3");																
-            break;
-        default:
-            break;
-    }
+   // Store filtered courses
+       let filteredCourses = courses.courses.filter(course =>
+           course.name.toLowerCase().includes(searchTerm)  // Check if the course name contains the search term
+		   &&
+           (selectedLevel === "All" || course.level === selectedLevel)
+       );
 
     // Select sort
     if (selectedSort === "Lowest to Highest") {
+		// Sort courses in ascending order
         filteredCourses.sort((a, b) => a.level - b.level);
     } else if (selectedSort === "Highest to Lowest") {
-        filteredCourses.sort((a, b) => b.level - a.level);
+		// Sort courses in descending order
+		filteredCourses.sort((a, b) => b.level - a.level);
     }
 
     // Get the course list container element
     let courseListContainer = document.getElementById('course-list');
     courseListContainer.innerHTML = '';
 
-    // Output the filtered courses to the course list container
-    filteredCourses.forEach(course => {
-        // Create a div element for each course card
-        let courseCard = document.createElement('div');
-        courseCard.classList.add('course-card');
+	// Generate HTML markup for each course block
+	let courseBlockHTML = filteredCourses.map(course => `
+    <div class="course-block">
+        <img src="Images/${course.image}" alt="${course.name}">
+        <h2>${course.name}</h2>
+        <p>Level ${course.level}</p>
+        <p>Code: ${course.code}</p>
+        <p class="small-description">Description: ${course.description}</p>
+    </div>
+	`).join('');
 
-        // Create an image element for the course image
-        let courseImage = document.createElement('img');
-        courseImage.src = "Images/" + course.image;
-        courseImage.alt = course.name;
-        courseCard.appendChild(courseImage);
+	// Add the course 
+	courseListContainer.innerHTML = courseBlockHTML;
 
-        // Create a heading element for the course name
-        let courseName = document.createElement('h2');
-        courseName.textContent = course.name;
-        courseCard.appendChild(courseName);
-
-        // Create a paragraph element for the course level
-        let courseLevel = document.createElement('p');
-        courseLevel.textContent = `Level ${course.level}`;
-        courseCard.appendChild(courseLevel);
-
-        // Create a paragraph element for the course code
-        let courseCode = document.createElement('p');
-        courseCode.textContent = `Code: ${course.code}`;
-        courseCard.appendChild(courseCode);
-
-        // Create a paragraph element for the course description
-        let courseDescription = document.createElement('p');
-        courseDescription.textContent = `Description: ${course.description}`;
-        courseCard.appendChild(courseDescription);
-
-        // Append the course card to the course list container
-        courseListContainer.appendChild(courseCard);
-    });
 
     event.preventDefault();
 }
 
-// Call the filterCourses function when the page is loaded
-window.addEventListener("load", filterCourses);
+// Call the selectCourse function when the page is loaded
+window.addEventListener("load",selectCourse);
