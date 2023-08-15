5b8930ba7c05b806436f99623cf5fc6992aaa673
ottawaAli <137909613+ottawaAli@users.noreply.github.com>
2023-08-15 10:09:41 -0400

    assignment2

--- Scripts/course.js
+++ Scripts/course.js
@@ -1,9 +1,8 @@
-// Course data
 const courses = {
-    "courses":[
+    "myCourses":[
         {"name":"Introduction to Computer Programming(Java)", "level":"1", "code":"CST7116", "description":"An introduction to computer programming with emphasis on problem analysis and design, using algorithms, pseudocode, flowcharts, UML class diagrams and testing, with the Java programming language used as a means to implement problem solution designs.", "image":"Lv1Java.jpg"},
         {"name":"Introduction to Database", "level":"1", "code":"CST7215", "description":"the fundamentals of relational databases design using Entity Relation Diagrams (ERDs), and use Structured Query Language (SQL) to create, modify and query a database. ", "image":"Lv1Data.jpg"},
-        {"name":"Computer Essentials", "level":"1", "code":"CST7101", "description":"Maintaining a computer, using operating system and productivity software, and expressing related environmental and sustainability concerns.", "image":"Lv1CE.jpg"},
+        {"name":"Computer Essentials", "level":"1", "code":"CST7101", "description":"Maintaining a computer, using operating system and productivity software, and expressing related environmental and sustainability concerns.", "image":"Lv1CE.JPG"},
         {"name":"Tech Math for Computer Science", "level":"1", "code":"MAT7100", "description":"Algebraic and transcendental functions, equations, linear systems, graphing functions, computer number systems, Boolean algebra and logic, and vector addition/subtraction. ", "image":"Math.png"},
         {"name":"College and Career Success", "level":"1", "code":"PRL7532", "description":"Awareness of self as a learner and study of learning, motivation and problem-solving theories. ", "image":"Success.png"},
         {"name":"Object-Oriented Programming(Java)", "level":"2", "code":"CST7284", "description":"Object oriented concepts, such as encapsulation, inheritance, abstraction and polymorphism are covered and reinforced with practical applications. The basics of data structures and algorithms as well as basic Graphical User Interface (GUI) programming.", "image":"Lv2Java.jpg"},
@@ -17,53 +16,85 @@ const courses = {
     ]
 }
 
-function selectCourse(event) {
-    // Get user's input, using toLowerCase() to implement case-insensitive search
-    let searchTerm = document.getElementById("search-course").value.toLowerCase();
+function filterCourses(event) {
+    // Get user's input 
+    let searchTerm = document.getElementById("query").value;
     let selectedLevel = document.getElementById("filter-level").value;
     let selectedSort = document.getElementById("sort-level").value;
 
-   // Store filtered courses
-       let filteredCourses = courses.courses.filter(course =>
-           course.name.toLowerCase().includes(searchTerm)  // Check if the course name contains the search term
-		   &&
-           (selectedLevel === "All" || course.level === selectedLevel)
-       );
+    // Store  courses
+    let filteredCourses = [];
+
+    // Select the level
+    switch (selectedLevel) {
+        case "All":
+            filteredCourses = courses.myCourses.filter(course => course.name.toLowerCase().includes(searchTerm.toLowerCase()));
+            break;
+        case "Level 1":
+            filteredCourses = courses.myCourses.filter(course => course.name.toLowerCase().includes(searchTerm.toLowerCase())
+                                                                && course.level === "1");
+            break;
+        case "Level 2":
+            filteredCourses = courses.myCourses.filter(course => course.name.toLowerCase().includes(searchTerm.toLowerCase())
+                                                                && course.level === "2");
+            break;																
+        case "Level 3":																
+            filteredCourses = courses.myCourses.filter(course => course.name.toLowerCase().includes(searchTerm.toLowerCase())
+                                                                && course.level === "3");																
+            break;
+        default:
+            break;
+    }
 
     // Select sort
     if (selectedSort === "Lowest to Highest") {
-		// Sort courses in ascending order
         filteredCourses.sort((a, b) => a.level - b.level);
     } else if (selectedSort === "Highest to Lowest") {
-		// Sort courses in descending order
-		filteredCourses.sort((a, b) => b.level - a.level);
+        filteredCourses.sort((a, b) => b.level - a.level);
     }
 
     // Get the course list container element
     let courseListContainer = document.getElementById('course-list');
     courseListContainer.innerHTML = '';
 
-	// Generate HTML markup for each course block
-	let courseBlockHTML = filteredCourses.map(course => `
-    <div class="course-block">
-        <div class="course-image">
-            <img src="Images/${course.image}" alt="${course.name}">
-        </div>
-        <div class="course-details">
-            <h2>${course.name}</h2>
-            <p>Level ${course.level}</p>
-            <p>Code: ${course.code}</p>
-            <p class="small-description">Description: ${course.description}</p>
-        </div>
-    </div>
-	`).join('');
+    // Output the filtered courses to the course list container
+    filteredCourses.forEach(course => {
+        // Create a div element for each course card
+        let courseCard = document.createElement('div');
+        courseCard.classList.add('course-card');
 
-	// Add the course 
-	courseListContainer.innerHTML = courseBlockHTML;
+        // Create an image element for the course image
+        let courseImage = document.createElement('img');
+        courseImage.src = "Images/" + course.image;
+        courseImage.alt = course.name;
+        courseCard.appendChild(courseImage);
 
+        // Create a heading element for the course name
+        let courseName = document.createElement('h2');
+        courseName.textContent = course.name;
+        courseCard.appendChild(courseName);
+
+        // Create a paragraph element for the course level
+        let courseLevel = document.createElement('p');
+        courseLevel.textContent = `Level ${course.level}`;
+        courseCard.appendChild(courseLevel);
+
+        // Create a paragraph element for the course code
+        let courseCode = document.createElement('p');
+        courseCode.textContent = `Code: ${course.code}`;
+        courseCard.appendChild(courseCode);
+
+        // Create a paragraph element for the course description
+        let courseDescription = document.createElement('p');
+        courseDescription.textContent = `Description: ${course.description}`;
+        courseCard.appendChild(courseDescription);
+
+        // Append the course card to the course list container
+        courseListContainer.appendChild(courseCard);
+    });
 
     event.preventDefault();
 }
 
-// Call the selectCourse function when the page is loaded
-window.addEventListener("load",selectCourse);
+// Call the filterCourses function when the page is loaded
+window.addEventListener("load", filterCourses);
